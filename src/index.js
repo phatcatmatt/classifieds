4c40d517f3de2e9e1dc633bdaf223cb3d847b489
Matt Nicholas <matt@mattnicholas.io>
2018-06-30 14:18:12 -0400

    added apollo and call to get data from graphcool db

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from 'apollo-boost';
+import config from './config';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: config.listingsAPI,
+})
+
+
+client.query({
+  query: gql`
+    {
+      allListings {
+        title
+        price
+        category
+      }
+    }
+  `
+})
+.then(res => console.log('api res', res));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
