e01cf72bc9f04d90ea753650a14b92a2aa97dfa5
Matt Nicholas <matt@mattnicholas.io>
2018-07-01 12:26:58 -0400

    wrapped app in apollo provider, call db to get list items and render them

--- src/components/classified-list-item.jsx
+++ src/components/classified-list-item.jsx
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+// apollo/graphql
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+// UI
 import './classified-list-item.css';
+
+const ListItem = () => (
+  <Query
+    query={gql`
+      {
+        allListings{
+          title
+          price
+          id
+        }
+      }
+    `}
+  >
+    {({loading, error, data}) => {
+      if (loading) return <p>loading...</p>;
+      if (error) return <p>error loading listings</p>
+
+      return data.allListings.map(({title, price, id}) => (
+        <div key={id}>
+          <p>{`${title} - $${price}`}</p>
+        </div>
+      ))
+    }}
+  </Query>
+)
 
 class ClassifiedListItem extends Component {
   render() {
     return (
-      <div className="list-item">
-        <h1>Post Title</h1>
-        <h2>$price</h2>
-        <h3>Location of Item</h3>
-        <h3>Item category</h3>
-        {/* example button from ant design*/}
-        <Button type="primary">Button</Button>
-      </div>
+      <ListItem/>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,27 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import config from './config';
-import gql from 'graphql-tag';
 
 const client = new ApolloClient({
   uri: config.listingsAPI,
 })
 
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
 
-client.query({
-  query: gql`
-    {
-      allListings {
-        title
-        price
-        category
-      }
-    }
-  `
-})
-.then(res => console.log('api res', res));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 registerServiceWorker();
